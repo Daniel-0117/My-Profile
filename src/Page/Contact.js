21f31e623cf2747b0ca122d846400e4384c22c57
Daniel-0117 <nevaslon0117@gmail.com>
2023-08-22 12:16:11 -0700

    finished contact

--- src/Page/Contact.js
+++ src/Page/Contact.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Contact = () => {
-  return <form >
+  const [formState, setFormState] = useState({name: '', email: '', message: ''});
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setFormState({...formState, [e.target.name]: e.target.value});
+  };
+
+  return ( 
+  <form >
     <div className="mb-3">
   <label htmlFor="name" class="form-label">Full Name</label>
-  <input type="text" name='name' className="form-control"  placeholder="Enter name here" />
+  <input type="text" name='name' className="form-control"  placeholder="Enter name here" defaultValue={formState.name} onChange={handleChange} />
 </div>
 <div className="mb-3">
   <label htmlFor="email" class="form-label">Email address</label>
-  <input type="email" name='email' className="form-control" placeholder="name@example.com" />
+  <input type="email" name='email' className="form-control" placeholder="name@example.com" defaultValue={formState.email} onChange={handleChange} />
 </div>
 <div className="mb-3">
   <label htmlFor="messagw" className="form-label">Message</label>
-  <textarea name='message' className="form-control" rows="3"></textarea>
+  <textarea name='message' className="form-control" rows="3" defaultValue={formState.message} onChange={handleChange} ></textarea>
 </div>
 <button type="submit" className="btn btn-primary">Submit</button>  
   </form>
+  );
 };
 
 export default Contact;
